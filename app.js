81f3bd07b5eade509c9466174d781f981ba42d56
ksamrat224 <ksamrat224@gmail.com>
2025-01-09 22:09:57 +0545

    props

--- app.js
+++ app.js
@@ -1,67 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = ()=> {
-    return (
-        <div className="header">
-            <div className="logo-container">
-            <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt="Logo" />
-            </div>
-            <div className="nav-items">
-              <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us </li>
-                <li>Cart</li>
-              </ul>
-            </div>
-        </div>
-
-    );
-}
-
-const RestaurantCard = () => {
+const Header = () => {
   return (
-      <div className="res-card"> 
+    <div className="header">
+      <div className="logo-container">
         <img
-        className="res-logo"  
-        alt="biryani" src="https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg"/>
-        <h3>Meghana Foods</h3>
-        <h4>Biryani,North Indian,Asia</h4>
-        {/* <h4>4.4 stars</h4>
-        <h4>35 Minutes</h4> */}
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
       </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
-}
+};
+
+const RestaurantCard = (props) => {
+  return (
+    <div className="restro-card">
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/e0vvulfbahjxjz6k4uwi"
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.rating}</h4>
+      <h4>{props.time}</h4>
+    </div>
+  );
+};
 
 const Body = () => {
   return (
-    <div className="body ">
+    <div className="body">
       <div className="search">Search</div>
-      <div className="res-container"> </div>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+      <div className="res-container">
+        <RestaurantCard resName="Meghana Foods" cuisine="Biryani,North Indian,Asian" rating="4.4 stars" time="38 minutes"/>
+        <RestaurantCard resName="KFC" cuisine="Burger,South American" rating="4.5 stars" time="40 minutes "/>
+        <RestaurantCard />
+        
+      </div>
     </div>
   );
-}
+};
 
 const AppLayout = () => {
   return (
-     <div className="app">
-      <Header/> 
-      <Body/>
-     </div>
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
   );
-}
+};
 
-
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
