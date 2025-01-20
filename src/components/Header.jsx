2a2f0106899c0bc823bec377e420dcb8ec175055
ksamrat224 <ksamrat224@gmail.com>
2025-01-20 15:52:13 +0545

    episode6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
 const Header = () => {
+  const[btnNameReact,setBtnNameReact]= useState("login");
     return (
       <div className="header">
         <div className="logo-container">
@@ -15,6 +18,13 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login"
+            onClick={()=>{
+              btnNameReact=== "login"?setBtnNameReact("logout"):setBtnNameReact("login");
+            }}
+            >
+              {btnNameReact}
+            </button>
           </ul>
         </div>
       </div>
